40e7ebf6f2f34d0c73f5ac17aac85a6a6a7f8ad9
manasavijay09 <manasa.vijay09@gmail.com>
2024-02-14 20:34:05 -0600

    copied public developed folder from class repo 14 challenge

---
+++ public/js/blogpost.js
@@ -0,0 +1,47 @@
+const newFormHandler = async (event) => {
+  event.preventDefault();
+
+  const name = document.querySelector('#blogPost-name').value.trim();
+  const needed_funding = document.querySelector('#blogPost-funding').value.trim();
+  const description = document.querySelector('#blogPost-desc').value.trim();
+
+  if (name && needed_funding && description) {
+    const response = await fetch(`/api/blogPost`, {
+      method: 'POST',
+      body: JSON.stringify({ name, needed_funding, description }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to create blogPost');
+    }
+  }
+};
+
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+
+    const response = await fetch(`/api/blogPost/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to delete blogPost');
+    }
+  }
+};
+
+document
+  .querySelector('.new-blogPost-form')
+  .addEventListener('submit', newFormHandler);
+
+document
+  .querySelector('.blogPost-list')
+  .addEventListener('click', delButtonHandler);
